3c75e8897b1fe5a07526fca67273fbcab652b9c3
swatkat1127 <swatkat1127@gmail.com>
2021-03-02 23:38:13 +0530

    font size changed

--- lib/constant.dart
+++ lib/constant.dart
@@ -9,5 +9,5 @@ const kLabelTextStyle = TextStyle(fontSize: 18.00, color: Colors.grey,);
 const kNumberTextStyle = TextStyle(fontSize: 50.0,fontWeight: FontWeight.w900,);
 const kLargeTextStyle = TextStyle(fontSize: 25.0, fontWeight: FontWeight.bold,);
 const kTitleTextStyle = TextStyle(fontSize: 50.0, fontWeight: FontWeight.bold,);
-const kResultTextStyle = TextStyle(fontSize: 20.0,color: Colors.green,fontWeight: FontWeight.bold,);
+const kResultTextStyle = TextStyle(fontSize: 25.0,color: Colors.green,fontWeight: FontWeight.bold,);
 const kBMITextStyle = TextStyle(fontSize: 100.0,color: Colors.white,fontWeight: FontWeight.bold,);
